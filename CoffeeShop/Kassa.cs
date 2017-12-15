f3f0bdfb48094fac475edbd90d2f1c3326e0c1be
Philippe Sormani <philippe.sormani201005@student.leeuwenborgh.nl>
2017-12-15 11:58:18 +0100

    mentor aids zjwik

---
+++ CoffeeShop/Kassa.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CoffeeShop
+{
+    public partial class Kassa : Form
+    {
+        public Kassa()
+        {
+            InitializeComponent();
+        }
+
+        private void Kassa_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ DataModel/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModel
+{
+    public class Hasj
+    {
+        private string naam;
+        private decimal prijs;
+        private decimal gewicht;
+
+        public Hasj()
+        {
+
+        }
+    }
+}
